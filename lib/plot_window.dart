4a3d10cfc1ca8226b33a00cb671051a994736e35
Edouard Tavinor <edouardtavinor@gmail.com>
2015-10-22 18:38:28 +0200

    moved Color to own file

--- lib/plot_window.dart
+++ lib/plot_window.dart
@@ -11,6 +11,7 @@ import "dart:math" as math;
 
 part "line.dart";
 part "axis.dart";
+part "color.dart";
 
 // TODO: Export any libraries intended for clients of this package.
 
@@ -19,24 +20,6 @@ class _PlotRectangle {
   Rectangle xy;
   Rectangle xy2;
   
-}
-
-class Color {
-  int red;
-  int green;
-  int blue;
-
-  static Color BLACK = new Color(0,0,0);
-  static Color WHITE = new Color(255,255,255);
-  
-  String toString()=>"#${red.toRadixString(16).padLeft(2, '0')}${green.toRadixString(16).padLeft(2, '0')}${blue.toRadixString(16).padLeft(2,'0')}";
-  
-  Color(int red, int green, int blue) {
-    if (red <0 || red >255) red = 0;
-    if (green <0 || green >255) green = 0;
-    if (blue <0 || blue >255) blue = 0;
-    this.red = red; this.green = green; this.blue = blue;
-  }
 }
 
 class PlotWindow {
